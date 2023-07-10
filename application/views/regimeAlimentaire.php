d41169043734b0e75ae5e33f806f222f9efd9d65
JossElito179 <josselito.303@gmail.com>
2023-07-10 20:25:50 +0300

    Front-hand of RegimeAlimentaire without php fetch

---
+++ application/views/regimeAlimentaire.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Regime Donnee</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> 
+
+  <link rel="stylesheet" href="<?= base_url().'/assets/css/ComplementTable.css'?>">
+
+</head>
+
+<body>
+<div class="card" id="main-table">
+    <div class="card-body">
+      <h5 class="card-title">Regime a suivre</h5>
+
+      <!-- Table with stripped rows -->
+      <table class="table table-striped">
+        <thead>
+          <tr class="titre">
+            <th scope="col">Jour</th>
+            <th scope="col">Fruit</th>
+            <th scope="col">Sucre</th>
+            <th scope="col">Legume</th>
+            <th scope="col">Accompagnement</th>
+            <th scope="col">Proteine</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Ananas 150g</td>
+            <td>Miel 50g</td>
+            <td>Poirreaux 140g</td>
+            <td>Riz 20g</td>
+            <td>Porc 30g</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Ananas 150g</td>
+            <td>Miel 50g</td>
+            <td>Poirreaux 140g</td>
+            <td>Riz 20g</td>
+            <td>Porc 30g</td>
+            </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Ananas 150g</td>
+            <td>Miel 50g</td>
+            <td>Poirreaux 140g</td>
+            <td>Riz 20g</td>
+            <td>Porc 30g</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Ananas 150g</td>
+            <td>Miel 50g</td>
+            <td>Poirreaux 140g</td>
+            <td>Riz 20g</td>
+            <td>Porc 30g</td>         
+            </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Ananas 150g</td>
+            <td>Miel 50g</td>
+            <td>Poirreaux 140g</td>
+            <td>Riz 20g</td>
+            <td>Porc 30g</td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- End Table with stripped rows -->
+
+    </div>
+  </div>
+</body>
+</html>
